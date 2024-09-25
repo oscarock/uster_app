0590f85561537e19e92682a940059823c4cb36ac
Oscar Diaz <felipe.diaz@linktic.com>
2024-09-24 20:31:17 -0500

    Crea las rutas de la aplicacion

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,16 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\VehicleController;
+use App\Http\Controllers\DriverController;
+use App\Http\Controllers\TripController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('vehicles', VehicleController::class)->except(['edit', 'update', 'show']);
+Route::resource('drivers', DriverController::class)->except(['edit', 'update', 'show']);
+Route::resource('trips', TripController::class)->except(['show', 'edit', 'update']);
+Route::get('trips/create/step-two', [TripController::class, 'createStepTwo'])->name('trips.create.step.two');
+Route::get('trips/create/step-three', [TripController::class, 'createStepThree'])->name('trips.create.step.three');
